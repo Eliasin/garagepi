dc274910c3027b07a8449d2146c3f12089c97d13
Eliasin <steven.pham.8.2000@gmail.com>
2017-11-19 19:13:36 -0500

    Remove global variables

--- garagepi.py
+++ garagepi.py
@@ -37,33 +37,34 @@ def open_door():
     pass
 
 
-try:
-    trusted_keys = load_keyfile(sys.argv[0])
-except IOError as e:
-    print(e)
-    trusted_keys = []
-    exit(errno.EACCES)
+def main():
+    try:
+        trusted_keys = load_keyfile(sys.argv[0])
+    except IOError as e:
+        print(e)
+        trusted_keys = []
+        exit(errno.EACCES)
 
-uuid = "9d298d8d-06b4-4da5-b913-0440aa7b4c70"
-server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-server_sock.bind(("", 0))
-server_sock.listen(1)
-bluetooth.advertise_service(server_sock, "garagepi", uuid)
+    uuid = "9d298d8d-06b4-4da5-b913-0440aa7b4c70"
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    server_sock.bind(("", 0))
+    server_sock.listen(1)
+    bluetooth.advertise_service(server_sock, "garagepi", uuid)
 
-while True:
-    client_sock, client_address = server_sock.accept()
-    client_sock.settimeout(20)
-    print("Accepted connection from {}".format(client_address))
+    while True:
+        client_sock, client_address = server_sock.accept()
+        client_sock.settimeout(20)
+        print("Accepted connection from {}".format(client_address))
 
-    salt = bcrypt.gensalt()
-    client_sock.send(salt)
-    print("Sent challenge of {} to {}.".format(salt, client_address))
+        salt = bcrypt.gensalt()
+        client_sock.send(salt)
+        print("Sent challenge of {} to {}.".format(salt, client_address))
 
-    client_response = client_sock.recv(60)
-    if client_response is not None and verify_challenge(client_response, salt, trusted_keys):
-        print("Client {} completed challenge of {}.".format(client_address, salt))
-        open_door()
-    else:
-        print("Client {} failed challenge of {}.".format(client_address, salt))
+        client_response = client_sock.recv(60)
+        if client_response is not None and verify_challenge(client_response, salt, trusted_keys):
+            print("Client {} completed challenge of {}.".format(client_address, salt))
+            open_door()
+        else:
+            print("Client {} failed challenge of {}.".format(client_address, salt))
 
-    client_sock.close()
+        client_sock.close()
