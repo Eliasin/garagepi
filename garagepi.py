dcca73da256ad141c7237b5053bff49a02cf142a
Eliasin <steven.pham.8.2000@gmail.com>
2017-11-20 21:55:45 -0500

    Implement challenges fully

--- garagepi.py
+++ garagepi.py
@@ -5,7 +5,7 @@ import bcrypt
 from typing import List
 
 
-def print_error(*args, **kwargs):
+def print_error(*args, **kwargs) -> None:
     print(*args, file=sys.stderr, **kwargs)
 
 
@@ -20,15 +20,15 @@ if len(sys.argv) > 2:
 # noinspection PyProtectedMember
 def load_keyfile(keyfile: str) -> List[str]:
     with open(keyfile, "a+") as f:
-        keys = [key for key in f]
-    print("Successfully loaded keyfile {}".format(keyfile))
+        f.seek(0)
+        keys = [key.strip() for key in f]
+    print("Successfully loaded keyfile {} with keys {}".format(keyfile, keys))
     return keys
 
 
-def verify_challenge(response: str, salt: str, keys: List[str]):
+def verify_challenge(response: str, keys: List[str]) -> bool:
     for key in keys:
-        correct_response = bcrypt.hashpw(key.encode(), salt)
-        if bcrypt.checkpw(response.encode(), correct_response):
+        if bcrypt.checkpw(key.encode(), response):
             return True
     return False
 
@@ -59,18 +59,19 @@ def main():
         client_sock.settimeout(20)
         print("Accepted connection from {}".format(client_address))
 
-        salt = bcrypt.gensalt()
+        salt = bcrypt.gensalt(prefix=b"2a")
         client_sock.send(salt)
         print("Sent challenge of {} to {}.".format(salt, client_address))
 
         try:
             client_response = client_sock.recv(60)
-            if verify_challenge(client_response, salt, trusted_keys):
+            if verify_challenge(client_response, trusted_keys):
                 print("Client {} completed challenge of {}.".format(client_address, salt))
                 open_door()
             else:
                 print("Client {} failed challenge of {}.".format(client_address, salt))
-        except bluetooth.btcommon.BluetoothError:
+        except bluetooth.btcommon.BluetoothError as e:
+            print_error(e)
             print("Client {} timed out on challenge of {}.".format(client_address, salt))
 
         client_sock.close()
